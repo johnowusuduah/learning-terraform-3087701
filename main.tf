3d61762e6780b7b48c9012c3c8243a333df08ca2
John Owusu Duah <67676957+johnowusuduah@users.noreply.github.com>
2024-03-06 16:49:47 -0500

    update entire load balancer

--- main.tf
+++ main.tf
@@ -46,27 +46,26 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name    = "blog-alb"
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-   target_groups = {
-    my_target = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
