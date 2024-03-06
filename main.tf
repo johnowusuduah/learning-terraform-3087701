d4ae7224132b4d84c622cebf204e5ca56bb5399d
John Owusu Duah <67676957+johnowusuduah@users.noreply.github.com>
2024-03-06 14:59:28 -0500

    chore: update target_id name in load balancer

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ module "alb" {
       target_type      = "instance"
       targets          = {
         my_target = {
-          target id = aws_instance.blog.id
+          target_id = aws_instance.blog.id
           port      = 80
         }
       }
