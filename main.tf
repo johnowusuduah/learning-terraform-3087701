65e0ba1d5f78343dede1a6c7a2c54f718d6368f5
John Owusu Duah <67676957+johnowusuduah@users.noreply.github.com>
2024-03-06 14:54:34 -0500

    chore: update targets of load balancer

--- main.tf
+++ main.tf
@@ -65,8 +65,10 @@ module "alb" {
       port             = 80
       target_type      = "instance"
       targets          = {
-        target id = aws_instance.blog.id
-        port      = 80
+        my_target = {
+          target id = aws_instance.blog.id
+          port      = 80
+        }
       }
     }
   }
